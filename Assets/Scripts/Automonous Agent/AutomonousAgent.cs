0b97561c0af2841974a5d53c810d29c3e8a38990
MartinMargolin <mmargolin@student.neumont.edu>
2023-01-10 20:52:26 -0700

    Agent and AutonomousAgent

---
+++ Assets/Scripts/Automonous Agent/AutomonousAgent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomonousAgent : Agent
+{
+    void Update()
+    {
+        var gameObjects = perception.GetGameObjects();
+        foreach (var gameObject in gameObjects)
+        {
+            Debug.DrawLine(transform.position, gameObject.transform.position);
+        }
+        if (gameObjects.Length > 0)
+        {
+            Vector3 direction = (gameObjects[0].transform.position - transform.position).normalized;
+            movement.ApplyForce(direction * 2);
+        }
+
+        transform.position = Utilities.Wrap(transform.position, Vector3.one * -10, Vector3.one * 10);
+    }
+}
